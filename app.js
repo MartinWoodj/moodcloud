c3022b1106475eeb721066f0ceb3f7207212288b
Joseph Wood <josephwood@Josephs-MacBook-Pro.local>
2015-11-28 22:09:52 -0500

    Fixed link not selecting correctly by name

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
+/*
+	$('a[name=angry]').on('click', '.mood', function(){
+		var moodName = $(this).attr('name');
+		console.log(moodName);
+	});
+*/
+/*
+	$('a[name=angry]').on('click', function(e){
+		var angryMood = $(this).attr('name');
+		console.log(angryMood);
+	});
+*/
+	
 	$('.player-bg').on('click', '.mood', function(){
 		var moodName = $(this).attr('name');
 		console.log(moodName);
 	});
+
+
 });
