211ee6a412b111ec8ef581aaa3119aaba9cbcb54
Joseph Wood <josephwood@Josephs-MacBook-Pro.local>
2015-12-09 00:29:36 -0500

    Coded background and center content box

--- app.js
+++ app.js
@@ -15,7 +15,26 @@ $(document).ready(function(){
 	$('.player-bg').on('click', '.mood', function(){
 		var moodName = $(this).attr('name');
 		console.log(moodName);
+		// find all tracks with the genre 'punk' that have a tempo greater than 120 bpm.
+		SC.get('/tracks', {
+			genres: moodName, bpm: { from: 120 }
+			}).then(function(tracks) {
+			console.log(tracks);
+		});
+		
+	});
+	
+	SC.initialize({ 
+	client_id: 'd0cf582a4733adb30dd7ebcbf05a82e4'
 	});
 
 
+	
+
+
 });
+
+
+
+
+
