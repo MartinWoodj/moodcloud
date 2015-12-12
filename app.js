ed5f7eeab3ff593f2171d46a3904537c6e357745
Joseph Wood <josephwood@Josephs-MacBook-Pro.local>
2015-12-12 12:56:39 -0500

    Working on iterating through array

--- app.js
+++ app.js
@@ -1,35 +1,28 @@
 $(document).ready(function(){
-/*
-	$('a[name=angry]').on('click', '.mood', function(){
-		var moodName = $(this).attr('name');
-		console.log(moodName);
-	});
-*/
-/*
-	$('a[name=angry]').on('click', function(e){
-		var angryMood = $(this).attr('name');
-		console.log(angryMood);
-	});
-*/
 	
 	$('.player-bg').on('click', '.mood', function(){
 		var moodName = $(this).attr('name');
 		console.log(moodName);
-		// find all tracks with the genre 'punk' that have a tempo greater than 120 bpm.
+		
 		SC.get('/tracks', {
 			genres: moodName, bpm: { from: 120 }
 			}).then(function(tracks) {
 			console.log(tracks);
 		});
 		
+		var processTracks = function(tracks) {
+			for (var i = 0; i < tracks.length; i++) {
+			console.log(tracks.title);
+  			}
+  			console.log(processTracks());
+		};
+		
 	});
+		
 	
 	SC.initialize({ 
 	client_id: 'd0cf582a4733adb30dd7ebcbf05a82e4'
 	});
-
-
-	
 
 
 });
